c2494a62e7fa0fb57aeb32b500426c7040eb0979
Jessica Sulaiman <jessica.sulaiman86@gmail.com>
2019-10-11 10:37:11 -0400

    services cards

--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -9,7 +9,7 @@ function Services() {
       <Row>
         <Col size="sm-12">
         <h1>Pet Services</h1>
-        <p>We welcome your pet at our comfy place at Sandy’s. We will be sure to accommodate your pets needs and hospitality.</p> 
+        <p>We welcome your pet at our comfy place at Sandy’s. We will be sure to accommodate your pets needs and hospitality. We take care of the following animals:</p> 
 
         <div className="card-deck">
         <div className="card">
@@ -18,11 +18,12 @@ function Services() {
           <div className="card-body">
             <h5 className="card-title">Dogs</h5>
             <ul>
-          <li>Walks</li>
+          <li>We take all dogs out for walks</li>
           <li>Playtime (Toys included!)</li>
-          <li>Training</li>
-          <li>Cuddles</li>
+          <li>Training if you'd like</li>
+          <li>Lots of cuddles</li>
           <li>Grooming</li>
+          <li>Treats!</li>
         </ul>
           </div>
         </div>
@@ -30,12 +31,12 @@ function Services() {
         <img src={`${images[3]}`} alt="banner" width="100%"/>
           <div className="card-body">
             <h5 className="card-title">Cats</h5>
-            <p>We keep our feline friends separate from our canine friends.</p>
           <ul>
           <li>Playtime (Toys included!)</li>
           <li>Cat nip</li>
           <li>Cuddles</li>
           <li>Grooming</li>
+          <li>Treats!</li>
         </ul>
           </div>
         </div>
@@ -45,27 +46,23 @@ function Services() {
             <h5 className="card-title">Small Animal (Hamsters, Guinea Pigs, Rats, Gerbils, Mice,Ferrets, Rabbits)</h5>
             <ul>
           <li>Hamsters will each receive 700sqft cage, 12inch wheel, obstacles, treats, they’ll never be bored with soft bedding.</li>
-          <li>Rabbits will snack on Timothy hay</li>
+          <li>Rabbits will snack on Timothy hay.</li>
           <li>All small animals will be seperate from reptiles, cats and dogs.</li>
         </ul>
           </div>
         </div>
       </div>
 
-{/*  */}
+<br/>
 <div className="card-deck">
         <div className="card">
         <img src={`${images[6]}`} alt="banner" width="100%"/>
-
           <div className="card-body">
             <h5 className="card-title">Reptiles (Snakes, Iguana, Lizards)</h5>
             <p>We provide appropriate environment standards, heat lamp etc.</p>
             <ul>
-          <li>Walks</li>
-          <li>Playtime (Toys included!)</li>
-          <li>Training</li>
-          <li>Cuddles</li>
-          <li>Grooming</li>
+          <li>We have several 30-gallon tanks for each reptile</li>
+          <li>Includes proper equipment such as lighting, lamps, bedding/substrate stc.</li>
         </ul>
           </div>
         </div>
@@ -73,23 +70,22 @@ function Services() {
         <img src={`${images[5]}`} alt="banner" width="100%"/>
           <div className="card-body">
             <h5 className="card-title">Birds</h5>
-            <p>We keep our feline friends separate from our canine friends.</p>
           <ul>
-          <li>Playtime (Toys included!)</li>
-          <li>Cat nip</li>
-          <li>Cuddles</li>
-          <li>Grooming</li>
+          <li>Appropriate cages depending on species</li>
+          <li>Toys</li>
+          <li>Hiding places</li>
+          <li>Proper food</li>
         </ul>
           </div>
         </div>
         <div className="card">
         <img src={`${images[7]}`} alt="banner" width="100%"/>
           <div className="card-body">
-            <h5 className="card-title">Small Animal (Hamsters, Guinea Pigs, Rats, Gerbils, Mice,Ferrets, Rabbits)</h5>
+            <h5 className="card-title">Fish (freshwater/saltwater)</h5>
             <ul>
-          <li>Hamsters will each receive 700sqft cage, 12inch wheel, obstacles, treats, they’ll never be bored with soft bedding.</li>
-          <li>Rabbits will snack on Timothy hay</li>
-          <li>All small animals will be seperate from reptiles, cats and dogs.</li>
+          <li>Moving to a different tank is stressful for any fish but our tanks are cycled for both freshwater and marine.</li>
+          <li>Tanks are limited to 5 gallons each</li>
+          <li>Hospital tanks and treatments are on stand by if needed.</li>
         </ul>
           </div>
         </div>
