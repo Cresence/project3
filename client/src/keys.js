375eec5c55d65929b6589cb598bd8c21ff96b9c1
Cresence <devharris93@gmail.com>
2019-10-28 20:25:16 -0400

    new keys

--- client/src/keys.js
+++ client/src/keys.js
@@ -1,3 +1,3 @@
 exports.adminEmail = {
-    id: ["devharris93@gmail.com", "dheeru.singh228@gmail.com", "jessica.sulaiman86@gmail.com"]
+    id: ["devharris93@gmail.com", "dheeru.singh228@gmail.com", "jessica.sulaiman86@gmail.com", "jessica.sulaiman21@gmail.com"]
 };
