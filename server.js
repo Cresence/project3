571add200058d8ec9b281a26865ff59c809f67c3
Cresence <devharris93@gmail.com>
2023-02-13 18:08:35 -0500

    Added console logs & catch error to mongo connection.

--- server.js
+++ server.js
@@ -146,7 +146,11 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || process.env.MY_MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || process.env.MY_MONGODB_URI).then(() => {
+  console.log("Connected to Database")
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+});;
 
 
 app.get("/api/images", (req, res) => {
