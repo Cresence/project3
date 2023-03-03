3a155cc5e8cc19a42430a61cb0a5a3ef81461283
Cresence <devharris93@gmail.com>
2023-03-03 04:52:42 -0500

    Cleaned up some code. Changing the logical operator needed the corresponding JSX code to be flipped.

--- client/src/components/Testimonial/index.js
+++ client/src/components/Testimonial/index.js
@@ -32,12 +32,9 @@ render(){
   return (
     <div id="myTestimonial" className="carousel slide" data-ride="carousel">
         {localTestimonials().length ? (
-            <h3 className="text-center">No Results to Display</h3>
-          ) : (
            	<div className="carousel-inner">
               {localTestimonials().map((testimonials, index) => (
-              
-                <div className={!index ? 'item carousel-item active' : 'item carousel-item'} key={testimonials._id}>
+                <div className={index.length ? 'item carousel-item active' : 'item carousel-item'} key={testimonials._id}>
                 <div className="img-box"><img src={`${testimonial_img}`} alt="" /></div>
                 <p className="testimonial">{testimonials.description}</p>
                 <p className="overview"><b>{testimonials.person_name}</b>, {testimonials.address}</p>
@@ -45,14 +42,14 @@ render(){
               ))}
             </div>
             
-          ) }
+          ) : ( <h3 className="text-center">No Results to Display</h3> )};
 			
 				<ol className="carousel-indicators">
-        {localTestimonials().length ? (
-            <h3 className="text-center">No Results to Display</h3>
-          ) : localTestimonials().map((testimonials, index) => (
+        {localTestimonials().length ? : localTestimonials().map((testimonials, index) => (
 					<li data-target="#myTestimonial" data-slide-to={index} className={!index ? 'active' : ''} key={testimonials._id}></li>
-          ))}
+          )) : (
+            <span>Nothing to see here.</span>
+          )};
         </ol>  
        
 		<a className="carousel-control left carousel-control-prev" href="#myTestimonial" data-slide="prev">
